e33f17bdbd0437e62c50da8a951421cfc20b3171
Tymur Naboka <112242837+Homa4@users.noreply.github.com>
2024-02-21 13:37:34 +0200

    connected month names
    (cherry picked from commit 9cf6b90fc23d55437fa6672617d9facbacc14560)

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+// global variables 
+const calendarWrapper = document.querySelector(".calendar-wrapper");
+const monthImg = document.querySelector(".img");
+const calendar = document.querySelector(".calendar");
+const monthName = document.querySelector(".monthName");
+const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+const weekDays = document.querySelector(".weekDays");
+const calendarTable = document.querySelector(".table");
+const tableBlock = document.querySelector(".block");
+const dayNumber = document.querySelector(".dayNumber");
 
+let today = new Date();
+let month = today.getMonth();
+let year = today.getFullYear();
+
+const monthArr = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+
+function initCalendar(){
+  const firstDay = new Date(year,month,1);
+  const firstDayNumber = firstDay.getDay();
+  const lastDay = new Date(year,month+1,0);
+  const lastDayDate = lastDay.getDate();
+
+
+  monthName.innerHTML = monthArr[month] + " " + year;
+
+  
+}
+
+initCalendar();
